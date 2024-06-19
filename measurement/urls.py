07556d004a3f7f054422009c98a5a43834d4242c
Andrew Liutov <87426517+andrewliutov@users.noreply.github.com>
2024-06-20 00:43:30 +0300

    Update urls.py

--- measurement/urls.py
+++ measurement/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from measurement.views import SensorsView, SensorView, MeasurementView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
     path('sensors/', SensorsView.as_view()),
     path('sensors/<pk>/', SensorView.as_view()),
     path('measurements/', MeasurementView.as_view()),
